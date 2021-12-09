57e1ac5d89786bdbe072381d03faa93dd961df8c
tuntub4 <89551281+tuntub4@users.noreply.github.com>
2021-12-09 15:34:52 -0500

    adjusted random stat

--- NYC_Bicycle_Analysis.py
+++ NYC_Bicycle_Analysis.py
@@ -50,7 +50,7 @@ def predict_raining(precipitation, totals, min_precip):
 
     is_raining = [0 if precip <= min_precip else 1 for precip in y]
 
-    [X_train, X_test, y_train, y_test] = train_test_split(X, is_raining, test_size=0.25, random_state=101)
+    [X_train, X_test, y_train, y_test] = train_test_split(X, is_raining, test_size=0.25, random_state=0)
 
     model = GaussianNB()
     model.fit(X_train, y_train)
